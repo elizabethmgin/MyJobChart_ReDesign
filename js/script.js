a31945e02cb8cbf2a3b8dcf34e91804d58866d2b
elizabethmgin <elizabethmgin@gmail.com>
2013-11-23 16:36:42 -0800

    cant get transitions to work...

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
 	$('a').live('click', function() {
+	    var clicked_link = $(this);
 		$.ajax({
 			dataType: "html",
 			url: $(this).attr('href'),
 			success: function(data){
-				console.dir(data);
-				$('section').html(data);
+			    if (clicked_link.hasClass('slideInLeft')){
+			        $('#container').after('<section id="transitionContainerLeft" style="position:absolute; width: 320px; right: 320px;"></section>');
+			        $('#transitionContainerLeft').html(data);
+			        $('#transitionContainerLeft').animate({right:0}, 500, function() {
+			            $('#container').remove();
+			            $('#transitionContainerLeft').attr('id','container');
+			        });
+			    }else {
+			        $('#container').html(data);
+			    }
 			}
 		});
 		return false;
