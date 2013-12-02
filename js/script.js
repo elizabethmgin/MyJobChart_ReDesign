19edaf303444c02ae24a19bc58a4927a529ada7d
elizabethmgin <elizabethmgin@gmail.com>
2013-12-02 15:04:32 -0800

    adding transitions

--- js/script.js
+++ js/script.js
@@ -6,13 +6,29 @@ $(document).ready(function() {
 			url: $(this).attr('href'),
 			success: function(data){
 			    if (clicked_link.hasClass('slideInLeft')){
-			        $('#container').after('<section id="transitionContainerLeft" style="position:absolute; width: 320px; right: 320px;"></section>');
+			        $('#container').after('<section id="transitionContainerLeft" style="position:absolute; width: 320px; height: 568px; right: 320px; top: 0; background-color: rgb(230,231,232);"></section>');
 			        $('#transitionContainerLeft').html(data);
 			        $('#transitionContainerLeft').animate({right:0}, 500, function() {
 			            $('#container').remove();
 			            $('#transitionContainerLeft').attr('id','container');
 			        });
-			    }else {
+			    }else if (clicked_link.hasClass('slideInRight')){
+    			    $('#container').after('<section id="transitionContainerRight" style="position:absolute; width: 320px; height: 568px; left: 320px; top: 0; background-color: rgb(230,231,232);"></section>');
+    			    $('#transitionContainerRight').html(data);
+    			    $('#transitionContainerRight').animate({left:0}, 500, function() {
+    			        $('#container').remove();
+    			        $('#transitionContainerRight').attr('id','container');
+    			    });
+    			    
+    			}else if (clicked_link.hasClass('slideInBottom')){
+        		    $('#container').after('<section id="transitionContainerBottom" style="position:absolute; width: 320px; height: 568px; left: 0; top: 568px; background-color: rgba(230,231,232, .5);"></section>');
+        		    $('#transitionContainerBottom').html(data);
+        		    $('#transitionContainerBottom').animate({top:0}, 500, function() {
+        		        $('#container').attr('id','background');
+        		        $('#transitionContainerBottom').attr('id','container');
+        		    });
+        		}  else {
+        		    $('#background').remove();
 			        $('#container').html(data);
 			    }
 			}
