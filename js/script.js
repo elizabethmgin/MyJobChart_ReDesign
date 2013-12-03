e1b034f733a25311934f89b697a6e716265a38d9
elizabethmgin <elizabethmgin@gmail.com>
2013-12-02 21:42:12 -0800

    job icons changed, range input fixed, sortable and draggable do not work

--- js/script.js
+++ js/script.js
@@ -13,10 +13,11 @@ $(document).ready(function() {
 			            $('#transitionContainerLeft').attr('id','container');
 			        });
 			    }else if (clicked_link.hasClass('slideInRight')){
-    			    $('#container').after('<section id="transitionContainerRight" style="position:absolute; width: 320px; height: 568px; left: 320px; top: 0; background-color: rgb(230,231,232);"></section>');
+    			    $('#container').after('<section id="transitionContainerRight" style="position:fixed; width: 320px; height: 568px; left: 320px; top: 0; background-color: rgb(230,231,232);"></section>');
     			    $('#transitionContainerRight').html(data);
     			    $('#transitionContainerRight').animate({left:0}, 500, function() {
     			        $('#container').remove();
+    			        $('#transitionContainerRight').attr('style','');
     			        $('#transitionContainerRight').attr('id','container');
     			    });
     			    
