64735e1f08b75e1f445eeeef461d99fe65cc9ad9
elizabethmgin <elizabethmgin@gmail.com>
2013-12-02 17:54:55 -0800

    all transitions working

--- js/script.js
+++ js/script.js
@@ -24,12 +24,28 @@ $(document).ready(function() {
         		    $('#container').after('<section id="transitionContainerBottom" style="position:absolute; width: 320px; height: 568px; left: 0; top: 568px; background-color: rgba(230,231,232, .5);"></section>');
         		    $('#transitionContainerBottom').html(data);
         		    $('#transitionContainerBottom').animate({top:0}, 500, function() {
+        		        $('.settingsSlide').remove();
         		        $('#container').attr('id','background');
         		        $('#transitionContainerBottom').attr('id','container');
+        		        $('#container').attr('class','addSlide');
         		    });
-        		}  else {
+        		}else if (clicked_link.hasClass('slideInTop')){
+            	    $('#container').after('<section id="transitionContainerTop" style="position:absolute; width: 320px; height: 500px; left: 0; top: -568px;"></section>');
+            	    $('#transitionContainerTop').html(data);
+            	    $('#transitionContainerTop').animate({top:35}, 500, function() {
+            	        $('.addSlide').remove();
+            	        $('#container').attr('id','background');
+            	        $('#transitionContainerTop').attr('id','container');
+            	        $('#container').attr('class','settingsSlide');
+            	    });
+            	}else if (clicked_link.hasClass('minimize')){
+                    $('#container').remove();
+                    $('#background').attr('id','container');
+                }else {
         		    $('#background').remove();
 			        $('#container').html(data);
+			        $('#container').css('top','0');
+			        $('#container').removeClass('settingsSlide addSlide');
 			    }
 			}
 		});
