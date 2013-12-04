cf9bb140d50f4db31616918435691648cc6f393f
elizabethmgin <elizabethmgin@gmail.com>
2013-12-03 19:06:46 -0800

    draggable and sortable working!

--- js/script.js
+++ js/script.js
@@ -6,14 +6,15 @@ $(document).ready(function() {
 			url: $(this).attr('href'),
 			success: function(data){
 			    if (clicked_link.hasClass('slideInLeft')){
-			        $('#container').after('<section id="transitionContainerLeft" style="position:absolute; width: 320px; height: 568px; right: 320px; top: 0; background-color: rgb(230,231,232);"></section>');
+			        $('#container').after('<section id="transitionContainerLeft" style="position:fixed; width: 320px; height: 568px; right: 320px; top: 0px; background-color: rgb(230,231,232);"></section>');
 			        $('#transitionContainerLeft').html(data);
 			        $('#transitionContainerLeft').animate({right:0}, 500, function() {
 			            $('#container').remove();
+			            $('#transitionContainerLeft').attr('style','');
 			            $('#transitionContainerLeft').attr('id','container');
 			        });
 			    }else if (clicked_link.hasClass('slideInRight')){
-    			    $('#container').after('<section id="transitionContainerRight" style="position:fixed; width: 320px; height: 568px; left: 320px; top: 20px; background-color: rgb(230,231,232);"></section>');
+    			    $('#container').after('<section id="transitionContainerRight" style="position:fixed; width: 320px; height: 568px; left: 320px; top: 0px; background-color: rgb(230,231,232);"></section>');
     			    $('#transitionContainerRight').html(data);
     			    $('#transitionContainerRight').animate({left:0}, 500, function() {
     			        $('#container').remove();
@@ -24,24 +25,24 @@ $(document).ready(function() {
     			}else if (clicked_link.hasClass('slideInBottom')){
         		    $('#container').after('<section id="transitionContainerBottom" style="position:absolute; width: 320px; height: 568px; left: 0; top: 568px;"></section>');
         		    $('#transitionContainerBottom').html(data);
+        		    $('#container').attr('id','background');
+        		    $( "#background" ).animate({opacity: 0.25}, 200, function() {
+                        // Animation complete.
+                      });
         		    $('#transitionContainerBottom').animate({top:0}, 500, function() {
         		        $('.settingsSlide').remove();
-        		        $('#container').attr('id','background');
-        		        $( "#background" ).animate({opacity: 0.25}, 200, function() {
-                            // Animation complete.
-                          });
         		        $('#transitionContainerBottom').attr('id','container');
         		        $('#container').attr('class','addSlide');
         		    });
         		}else if (clicked_link.hasClass('slideInTop')){
             	    $('#container').after('<section id="transitionContainerTop" style="position:absolute; width: 320px; height: 500px; left: 0; top: -568px;"></section>');
             	    $('#transitionContainerTop').html(data);
+            	    $('#container').attr('id','background');
+            	    $( "#background" ).animate({opacity: 0.25}, 200, function() {
+                        // Animation complete.
+                      });
             	    $('#transitionContainerTop').animate({top:35}, 500, function() {
             	        $('.addSlide').remove();
-            	        $('#container').attr('id','background');
-            	        $( "#background" ).animate({opacity: 0.25}, 200, function() {
-                            // Animation complete.
-                          });
             	        $('#transitionContainerTop').attr('id','container');
             	        $('#container').attr('class','settingsSlide');
             	    });
@@ -51,7 +52,7 @@ $(document).ready(function() {
                 }else {
         		    $('#background').remove();
 			        $('#container').html(data);
-			        $('#container').css('top','0');
+			        $('#container').attr('style','top:0;');
 			        $('#container').removeClass('settingsSlide addSlide');
 			    }
 			}
